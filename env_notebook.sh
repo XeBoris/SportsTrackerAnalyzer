1ea2f8dc1b1f79144b267168c914b4b2f90322cb
XeBoris <Boris.Bauermeister@gmail.com>
2020-10-03 09:46:48 +0200

    alter the notebook server start

--- env_notebook.sh
+++ env_notebook.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-source $PWD/env_start.sh
-jupyter-notebook --config $PWD/Notebooks/config/jupyter_notebook_config.py
+#source $PWD/env_start.sh
+#jupyter-notebook --config $PWD/Notebooks/config/jupyter_notebook_config.py
+
+#Start a notebook and leave it running on TMUX
+tmux new -d -s jup 'source $PWD/env_start.sh; jupyter-notebook --config $PWD/Notebooks/config/jupyter_notebook_config.py' \;
