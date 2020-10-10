e7691a63f04d3bca783f45f51366f6f9f7b47c1e
XeBoris <Boris.Bauermeister@gmail.com>
2020-10-10 09:46:21 +0200

    fix timestamp issue

--- sportstrackeranalyzer/module/strava.py
+++ sportstrackeranalyzer/module/strava.py
@@ -5,6 +5,7 @@ import gpxpy
 import gpxpy.gpx
 import datetime
 import hashlib
+import numpy as np
 import pandas as pd
 
 from .db_handler import DataBaseHandler
@@ -101,8 +102,8 @@ class Strava():
         #Import a single gpx file here:
         self.load_gps()
 
-        beg_branch = int(min(self.df["timestamp"]).replace(tzinfo=datetime.timezone.utc).timestamp()*1000)
-        end_branch = int(max(self.df["timestamp"]).replace(tzinfo=datetime.timezone.utc).timestamp()*1000)
+        beg_branch = int(min(self.df["timestamp"])*1000)
+        end_branch = int(max(self.df["timestamp"])*1000)
 
         #Load the gpx file and create the branch/track info
         blueprint_session = self.bp.Session()
@@ -112,16 +113,27 @@ class Strava():
         blueprint_session["updated_at"] = end_branch
         blueprint_session["title"] = self.track_name
         blueprint_session["notes"] = self.track_name
-        blueprint_session["start_time_timezone_offset"] = 7200000
-        blueprint_session["end_time_timezone_offset"] = 7200000
+        blueprint_session["start_time_timezone_offset"] = None
+        blueprint_session["end_time_timezone_offset"] = None
         blueprint_session["sports_type"] = None
-        blueprint_session["source"] = "Strave-GPS"
+        blueprint_session["source"] = "StravaGPS"
 
         if blueprint_session["sports_type"] is None:
             print("Activities are cycling, mountainbike, hiking, walking")
             blueprint_session["sports_type"] = input("State your activity: ")
         if blueprint_session["sports_type"] is None:
             print("Sorry this is wrong")
+            exit()
+
+        if blueprint_session["start_time_timezone_offset"] is None:
+            print("Add time offset between UTC and timezone when tour was made:")
+            print("Allowed only in NANOSECONDs")
+            offset = input("Offset [ns]: ")
+            blueprint_session["start_time_timezone_offset"] = offset
+            blueprint_session["end_time_timezone_offset"] = offset
+
+        if blueprint_session["start_time_timezone_offset"] is None:
+            print("something wrong")
             exit()
 
         # We add a track_hash to each track to make it unique:
@@ -179,11 +191,10 @@ class Strava():
                     self.obj_gps["timestamp"].append(point.time)
 
         self.df = pd.DataFrame(self.obj_gps)
-        self.df["timestamp"] = pd.to_datetime(self.df["timestamp"])
 
-
-
-
+        #Adjust the GPX timestamp from Strava:
+        self.df["timestamp"] = pd.to_datetime(self.df["timestamp"]) #Get rid of +Z
+        self.df["timestamp"] = pd.DatetimeIndex(self.df["timestamp"]).astype(np.int64)/1e9 #Convert it to seconds
 
     def _read_json(self, fjson):
         """
@@ -194,10 +205,4 @@ class Strava():
         gpx_file = open(self.gps_path, 'r')
         tree = ET.parse(self.gps_path)
         root = tree.getroot()
-        print(root)
-
-        for i in root:
-
-            for j in i:
-                print("d", j.text, j.tag)
         self.gpx = gpxpy.parse(gpx_file)
